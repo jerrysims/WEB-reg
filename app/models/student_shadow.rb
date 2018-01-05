b281b73bc95ab8db966e35a78250d817e3ca7404
Jerry Sims <jerrysims07@gmail.com>
2018-01-05 14:13:22 -0600

    Add new and edit student shadows

--- app/models/student_shadow.rb
+++ app/models/student_shadow.rb
@@ -6,18 +6,17 @@ class StudentShadow < ActiveRecord::Base
   validate :student_may_only_shadow_one_day
 
   def only_one_shadow_per_time_slot
-    # existing_ss = StudentShadow
     return if StudentShadow.where(shadow_spot: shadow_spot).empty?
     errors.add(:shadow_spot, "may only have one student at a time")
   end
 
   def student_may_only_shadow_one_day
-    spots = StudentShadow.where(student:student)
+    spots = StudentShadow.where(student: student)
     return true if spots.empty?
     if spots.count > 1
       errors.add(:student, "may not shadow more than twice")
     elsif
-      if spots.select { |s| s.shadow_spot.date != shadow_spot.date }.empty?
+      unless spots.select { |s| s.shadow_spot.date != shadow_spot.date }.empty?
         errors.add(:student, "may not shadow on separate dates")
       end
     end
