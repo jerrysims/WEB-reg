dd5e58b8f9fb4b565f35e554badf40d9207c9fab
Jerry Sims <jerrysims07@gmail.com>
2018-01-10 22:23:56 -0600

    Add logic around displaying of the shadow_spot selection box

--- app/controllers/student_shadows_controller.rb
+++ app/controllers/student_shadows_controller.rb
@@ -1,12 +1,15 @@
 class StudentShadowsController < ApplicationController
   def create
     @student = Student.find(params[:student_shadow][:student_id])
-    @student_shadow = StudentShadow.create(student_shadow_params)
-    if @student_shadow.errors[:student].include? 'may not shadow more than twice'
-      drop_schedule
+    unless params[:commit] == "Change Date"
       @student_shadow = StudentShadow.create(student_shadow_params)
-      @student.reload
+      if @student_shadow.errors[:student].include? 'may not shadow more than twice'
+        drop_schedule
+        @student_shadow = StudentShadow.create(student_shadow_params)
+        @student.reload
+      end
     end
+    @should_show_selection_box = get_should_show_selection_box(@student, params[:commit])
     @available_shadow_spots = ShadowSpot.select {
       |ss| ss.subject.grade == @student.grade } .select {
       |ss| !@student.shadow_spots.include?(ss)
@@ -27,6 +30,7 @@ class StudentShadowsController < ApplicationController
 
   def new
     @student = Student.find(params[:student_id]) unless params[:student_id].nil?
+    @should_show_selection_box = get_should_show_selection_box(@student)
     @available_shadow_spots = ShadowSpot.select {
       |ss| ss.subject.grade == @student.grade } .select {
       |ss| !@student.shadow_spots.include?(ss)
@@ -42,6 +46,18 @@ class StudentShadowsController < ApplicationController
   def drop_schedule
     @student.student_shadows.destroy_all
     @student.shadow_spots.destroy_all
+  end
+
+  def get_should_show_selection_box(student, commit = nil)
+    if (student.shadow_spots.count == 0 ||
+        commit == "Add Additional class"
+      )
+      true
+    elsif (student.shadow_spots.count == 2 && commit != "Change Date")
+      false
+    else
+      true
+    end
   end
 
 end
