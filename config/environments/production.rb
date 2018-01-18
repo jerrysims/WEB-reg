f30e03fc112884c9ee4a408a3cdef44b6c354127
Jerry Sims <jerrysims07@gmail.com>
2018-01-17 19:03:09 -0600

    Change email port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = {
     address: 'smtp.gmail.com',
-    port: 465
+    port: 587 
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
