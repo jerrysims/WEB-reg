0c526353bf54f3d55576f5bac84dba8811fa4989
Jerry Sims <jerrysims07@gmail.com>
2018-01-17 19:21:44 -0600

    email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,9 +63,12 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = {
+  config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
-    port: 587
+    port: 587,
+    domain: 'webtutorialnashville.com',
+    authentication: 'plain',
+    enable_starttls_auto: true
   }
   config.action_mailer.delivery_method = :smtp
 
