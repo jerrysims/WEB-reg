f5dd7144a8e14de92d7481aa701ee3fdcfc09fd6
Jerry Sims <jerrysims07@gmail.com>
2018-01-21 15:53:02 -0600

    Update Active Admin pages

---
+++ app/admin/parents.rb
@@ -0,0 +1,38 @@
+ActiveAdmin.register Parent do
+  form do |f|
+    f.input :email
+    f.input :first_name
+    f.input :last_name
+    f.input :phone_number
+    f.input :password
+    f.input :password_confirmation
+    actions
+  end
+  permit_params :first_name, :last_name, :email, :phone_number, :password, :password_confirmation
+
+  index do
+    column :email
+    column :first_name
+    column :last_name
+    column :phone_number
+    column :sign_in_count
+    column "# of Students" do |p|
+      p.students.count
+    end
+    actions
+  end
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+end
--- app/admin/teachers.rb
+++ app/admin/teachers.rb
@@ -1,9 +1,9 @@
-ActiveAdmin.register Student do
+ActiveAdmin.register Teacher do
   form do |f|
     f.input :first_name
     f.input :last_name
-    f.input :grade
-    f.input :parent
+    f.input :email
+    f.input :subject
     actions
   end
   permit_params :first_name, :last_name, :parent_first_name, :parent_last_name
@@ -11,9 +11,8 @@ ActiveAdmin.register Student do
   index do
     column :first_name
     column :last_name
-    column :grade
-    column :parent
-    column :lunch
+    column :email
+    column :subject
     actions
   end
 end
