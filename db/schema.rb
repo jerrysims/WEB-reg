235ea22d1c28ebe6ce2ba81ff85721c71fa37c7f
Jerry Sims <jerrysims07@gmail.com>
2018-03-13 13:01:19 -0500

    Add status attribute to Registration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180306222005) do
+ActiveRecord::Schema.define(version: 20180313174845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,13 @@ ActiveRecord::Schema.define(version: 20180306222005) do
   create_table "registrations", force: :cascade do |t|
     t.integer  "student_id"
     t.integer  "course_id"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.string   "status",     default: "selected"
+  end
+
+  create_table "schedules", force: :cascade do |t|
+    t.integer  "student_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
