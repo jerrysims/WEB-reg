8d7b1ab07ca1257194178fdcd98c23b09b669596
Jerry Sims <jerrysims07@gmail.com>
2018-03-15 12:20:55 -0500

    Add tests for Course model

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should have_many(:course_corequisites) }
+    it { should have_many(:corequisites).through(:course_corequisites) }
+    it { should have_many(:registrations) }
+    it { should have_many(:students).through(:registrations) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name)}
+    it { should validate_presence_of(:grades)}
+    it { should validate_presence_of(:start_time)}
+    it { should validate_presence_of(:start_time)}
+    it { should validate_presence_of(:day)}
+  end
 end
