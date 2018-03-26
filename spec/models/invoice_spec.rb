3e40a51c882fdb0b35afe26299ef461c20d7aba6
Jerry Sims <jerrysims07@gmail.com>
2018-03-25 19:32:08 -0500

    Add specs for invoice

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -9,4 +9,35 @@ RSpec.describe Invoice, type: :model do
   describe 'validations' do
   end
 
+  describe 'instance_methods' do
+    let(:parent) { create(:parent) }
+    let(:invoice) { create(:invoice) }
+    let(:product) { create(:product) }
+    let(:product_2) { create(:product, name: "Another Product") }
+
+    describe '#total_due' do
+      context 'when there are no line items in the invoice' do
+        it 'should return 0' do
+          expect(invoice.total_due).to eq(0)
+        end
+      end
+
+      context 'when there is one line item' do
+        let!(:invoice_line_item) { create(:invoice_line_item, product: product, invoice: invoice, parent: parent) }
+
+        it 'should return the value of that line item' do
+          expect(invoice.total_due).to eq(50)
+        end
+      end
+
+      context 'when there is more than one line item' do
+        let!(:invoice_line_item) { create(:invoice_line_item, product: product, invoice: invoice, parent: parent) }
+        let!(:invoice_line_item_2) { create(:invoice_line_item, product: product_2, invoice: invoice, parent: parent) }
+
+        it 'should return the sum of those two line items' do
+          expect(invoice.total_due).to eq(100)
+        end
+      end
+    end
+  end
 end
