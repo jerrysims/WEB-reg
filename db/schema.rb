662620c95c5a13c4957c9213459398e692b9507a
Jerry Sims <jerrysims07+github@gmail.com>
2019-03-07 11:56:57 -0600

    Add closed attribute to invoice model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_03_26_041920) do
+ActiveRecord::Schema.define(version: 2019_03_07_161122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2018_03_26_041920) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "parent_id"
+    t.boolean "closed", default: false
     t.index ["parent_id"], name: "index_invoices_on_parent_id"
   end
 
