a13b905b13f4490685a9dc6ae8cb85efa1f43466
Jerry Sims <jerrysims07+github@gmail.com>
2019-03-23 10:18:19 -0500

    Add student to invoice line items

--- app/admin/invoice_line_items.rb
+++ app/admin/invoice_line_items.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register InvoiceLineItem do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :parent_id, :invoice_id, :product_id, :quantity
+permit_params :parent_id, :student_id, :invoice_id, :product_id, :quantity
 #
 # or
 #
@@ -16,6 +16,7 @@ form do |f|
   f.inputs do
     f.input :invoice, as: :select, collection: options_from_collection_for_select(Invoice.all, :id, lambda { |i| "#{i.parent.full_name}"})
     f.input :parent
+    f.input :student
     f.input :product
     f.input :quantity
     actions
