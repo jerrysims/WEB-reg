d8538bdfc06a7f6edcb08e6c758152da162b2956
Jerry Sims <jerrysims07+github@gmail.com>
2019-03-28 12:51:30 -0500

    Add Class Rosters report

---
+++ app/admin/class_rosters.rb
@@ -0,0 +1,25 @@
+ActiveAdmin.register Course, as: "Class Rosters" do
+  menu parent: "Reports"
+
+  index do |course|
+    column :name
+    column do |course|
+      unless course.registrations.empty?
+        table_for course.registrations do
+          column "Class Roster" do |registration|
+            registration.student.full_name
+          end
+        end
+      end
+    end
+  end
+
+  csv do
+    column "Course Name / Time" do |c|
+      "#{c.name}, #{c.start_time}"
+    end
+    column "Students" do |c|
+      c.registrations.map { |r| r.student.full_name }
+    end
+  end
+end
