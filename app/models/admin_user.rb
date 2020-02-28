7445cac26484fa06942b79c1f03cd891090f48ea
Jerry Sims <jerrysims@jerrys-air.attlocal.net>
2020-02-28 00:01:47 -0600

    Add admin users

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,3 +1,16 @@
-class AdminUser < Parent 
+class AdminUser < Parent
   self.inheritance_column = :_type_disabled
+
+  after_create { |admin| admin.send_reset_password_instructions }
+
+  def password_required?
+    new_record? ? false : super
+  end
+
+  before_destroy :raise_if_last
+  def raise_if_last
+    if AdminUser.count < 2
+      raise "Can't delete last admin user"
+    end
+  end
 end
