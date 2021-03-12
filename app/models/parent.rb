6a528824b9f89cd92a457c23c268d584ed8b8590
Jerry Sims <jerrysims07@gmail.com>
2021-03-11 18:39:14 -0600

    Removes unused code

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,5 +1,5 @@
 class Parent < ActiveRecord::Base
-  rolify
+  # rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
