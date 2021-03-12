417bcde7acd124f433a75f740c26cc00dd7cfae8
Jerry Sims <jerrysims07@gmail.com>
2021-03-11 20:02:03 -0600

    Updates email for Mailers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
     port: 587,
     user_name: ENV['GMAIL_USERNAME'],
     password: ENV['GMAIL_PASSWORD'] ,
-    domain: 'webtutorialnashville.com',
+    domain: 'gmail.com',
     authentication: 'plain',
     enable_starttls_auto: true
   }
