aa3048301b02a8d8a68468eaa34c6027eab61416
Jerry Sims <jerrysims07@gmail.com>
2022-03-21 08:55:04 -0500

    Removes password from admin-parents#edit

--- app/admin/parents.rb
+++ app/admin/parents.rb
@@ -6,8 +6,6 @@ ActiveAdmin.register Parent do
     f.input :first_name
     f.input :last_name
     f.input :phone_number
-    f.input :password
-    f.input :password_confirmation
     actions
   end
   permit_params :first_name, :last_name, :email, :phone_number, :password, :password_confirmation
