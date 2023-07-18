91656f9025b80c03d03049d07bb17c69a3213521
Jerry Sims <jerrysims07+github@gmail.com>
2023-07-18 00:15:18 -0500

    Updates teachers page

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,12 +1,6 @@
 class TeachersController < ApplicationController
   before_action :set_teacher, only: :show
 
-  def parent_contact_list
-    @teacher = Teacher.find(params[:teacher_id])
-    @sections = @teacher.sections
-    @students = Student.where(id: Registration.includes(:section, :student).where(section: @teacher.sections).pluck(:student_id)).order(:last_name)
-  end
-
   def show
   end
 
