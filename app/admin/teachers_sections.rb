36488edb0d19c9e985d2c1db2dc0cbcba11c4f75
Jerry Sims <jerrysims07+github@gmail.com>
2024-02-14 13:34:25 -0600

    Adds link to main admin page

--- app/admin/teachers_sections.rb
+++ app/admin/teachers_sections.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register TeachersSection, as: "Teacher Assignments" do
+  menu parent: "Admin"
+
   form do |f|
     f.input :section, as: :select, collection: options_from_collection_for_select(Section.all.sort_by { |c| c.name }, :id, lambda { |c| "#{c.name}, #{c.day}, #{c.start_time.strftime("%l:%M")}"})
     f.input :teacher, as: :select, collection: options_from_collection_for_select(Teacher.all.sort_by { |t| t.last_name.downcase }, :id, lambda { |t| "#{t.full_name}"})
