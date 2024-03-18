5832a18968c19277354ff64e37a65327b838229f
Jerry Sims <jerrysims07+github@gmail.com>
2024-03-18 11:21:24 -0500

    Adds rp to accepted parameters in Products admin page

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Product do
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
 # permit_params :list, :of, :attributes, :on, :model
-permit_params :name, :unit_price, :product_type
+permit_params :name, :unit_price, :product_type, :registration_period_id
 #
 # or
 #
