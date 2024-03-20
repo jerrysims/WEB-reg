8de447de88e21504cd1dd069e78d3a649917fbef
Jerry Sims <jerrysims07+github@gmail.com>
2024-03-20 10:49:51 -0500

    Requires fields on Medical Form

--- app/models/medical_form.rb
+++ app/models/medical_form.rb
@@ -1,5 +1,13 @@
 class MedicalForm < ApplicationRecord
   belongs_to :student
+  validates_presence_of :emergency_contact_first_name,
+                        :emergency_contact_last_name,
+                        :emergency_contact_relationship,
+                        :preferred_hospital,
+                        :pain_reliever_permission,
+                        :medical_care_consent,
+                        :signature,
+                        :student_id
 
   def status
     return "complete" if emergency_contact_first_name && 
