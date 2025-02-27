ff1e41f8a3a42077ca936a012e228fb71d1a9af9
Jerry Sims <jerrysims07+github@gmail.com>
2025-02-27 12:32:38 -0600

    Updates Home view for parents with links to new enrollment forms and other useful links

--- app/models/registration_period.rb
+++ app/models/registration_period.rb
@@ -1,8 +1,9 @@
 class RegistrationPeriod < ApplicationRecord
-  CURRENT_RP = RegistrationPeriod.find_by(name: "2024-25 Academic Year")
+  CURRENT_RP = RegistrationPeriod.find_by(name: "2025-26 Academic Year")
   CURRENT_ACADEMIC_YEAR = RegistrationPeriod.find_by(name: "2024-25 Academic Year")
   RP_TYPES= %w(academic extracurricular)
-  RP_STATUSES = %W(future teachers returning all closed)
+  RP_STATUSES = %W(future teachers seniors juniors returning all closed)
+  
 
   validates_inclusion_of :rp_type, in: RP_TYPES
   validates_inclusion_of :status, in: RP_STATUSES
