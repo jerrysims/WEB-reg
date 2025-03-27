660f2edab8ac200dbf2059e02740a0739732fe77
Jerry Sims <jerrysims07+github@gmail.com>
2025-03-27 11:52:01 -0500

    Adds registration period name to dropdown box for WaitListedStudent admin page

--- app/admin/wait_listed_students.rb
+++ app/admin/wait_listed_students.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register WaitListedStudent do
   form do |f|
-    f.input :section, as: :select, collection: options_from_collection_for_select(Section.all.sort_by { |c| c.name }, :id, lambda { |c| "#{c.name}, #{c.day}, #{c.start_time.strftime("%l:%M")}"})
+    f.input :section, as: :select, collection: options_from_collection_for_select(Section.all.sort_by { |c| c.name }, :id, lambda { |c| "#{c.name}, #{c.day}, #{c.start_time.strftime("%l:%M")} (#{c.registration_period.name})" })
     f.input :student, as: :select, collection: options_from_collection_for_select(Student.all.sort_by { |s| s.last_name.downcase }, :id, lambda { |s| "#{s.full_name}"})
 
     actions
