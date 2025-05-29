f57fdea6dc74baa34efe8411048d4d5a10ef263b
Jerry Sims <jerrysims07+github@gmail.com>
2025-05-29 11:14:54 -0500

    Removes unnecessary filters on parent_emails report

--- app/admin/parent_emails.rb
+++ app/admin/parent_emails.rb
@@ -1,6 +1,20 @@
 ActiveAdmin.register Parent, as: "Parent Emails" do
   menu parent: "Reports"
 
+  # Remove all vanilla filters, keep only the custom Registration Period filter in the sidebar
+  config.filters = false
+
+  sidebar "Filter by Registration Period", only: :index do
+    form_tag admin_parent_emails_path, method: :get do
+      select_tag(
+        :registration_period_id,
+        options_from_collection_for_select(RegistrationPeriod.all.order(:name), :id, :name, params[:registration_period_id]),
+        include_blank: "All Registration Periods"
+      ) +
+      submit_tag("Filter", name: nil)
+    end
+  end
+
   index do |parent|
     column "Registered Parent" do |p|
       p.full_name
@@ -10,6 +24,17 @@ ActiveAdmin.register Parent, as: "Parent Emails" do
   end
 
   controller do
+    def scoped_collection
+      if params[:registration_period_id].present?
+        rp_id = params[:registration_period_id]
+        Parent.joins(students: { registrations: { section: { course: :registration_period } } })
+              .where(courses: { registration_period_id: rp_id })
+              .distinct
+      else
+        super
+      end
+    end
+
     def index
       if request.format == Mime[:xls]
         spreadsheet = ParentEmailsReport.new
