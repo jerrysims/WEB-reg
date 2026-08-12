f5a923b26fd42d2d5a73fcccfe09fe4976ca80e0
Jerry Sims <jerrysims07+github@gmail.com>
2026-08-12 14:04:52 -0500

    Adds student profiles link to Teacher Dashboard

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,6 +1,6 @@
 class AdminsController < ApplicationController
-  before_action :confirm_admin, except: [:student_profile]
-  before_action :confirm_admin_or_teacher, only: [:student_profile]
+  before_action :confirm_admin, except: [:student_profile, :students_schedules]
+  before_action :confirm_admin_or_teacher, only: [:student_profile, :students_schedules]
   before_action :set_student, only: [:student_schedule, :student_profile]
   before_action :set_rp, only: [:grades, :missing_documents, :student_schedule, :students_schedules, :view_all_grades]
   before_action :set_other_rps, only: [:grades, :view_all_grades]
